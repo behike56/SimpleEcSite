46491c273fefb95d7ea9da9f47810b344bd97671
behike56 <kc7u9k9tim3jfqtrrn3jykgecdwurk@gmail.com>
2019-04-05 19:49:10 +0900

    update: テーブル設計書

---
+++ resources/views/items/details.blade.php
@@ -0,0 +1,26 @@
+<!-- main -->
+<div class="col-md-9">
+    <!-- apply custom style -->
+    <div class="page-header" style="margin-top:-30px;padding-bottom:0px;">
+	<h1><small>タイトル</small></h1>
+    </div>
+    
+    <form class="form-horizontal">
+	<div class="form-group">
+	    <label for="name1" class="col-md-3 control-label">お名前</label>
+	    <div class="col-sm-9"><input type="text" class="form-control" id="name1"></div>
+	</div>
+	<div class="form-group">
+	    <label for="mail1" class="col-md-3 control-label">メールアドレス</label>
+	    <div class="col-sm-9"><input type="email" class="form-control" id="mail1"></div>
+	</div>
+	<div class="form-group">
+	    <label for="ask1" class="col-md-3 control-label">お問い合わせ内容</label>
+	    <div class="col-md-9"><textarea rows="5" class="form-control" id="ask1"></textarea></div>
+	</div>
+	
+	<div class="col-md-offset-3 text-center"><button class="btn btn-primary">送信</button></div>
+    </form>
+    
+</div>
+
