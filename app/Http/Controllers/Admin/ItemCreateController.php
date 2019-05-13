5603e52520884f7cdf4f1d3af9e11190562125ad
behike56 <kc7u9k9tim3jfqtrrn3jykgecdwurk@gmail.com>
2019-05-13 21:20:03 +0900

    deploy

--- app/Http/Controllers/Admin/ItemCreateController.php
+++ app/Http/Controllers/Admin/ItemCreateController.php
@@ -43,14 +43,8 @@ class ItemCreateController extends Controller
         $form = $request->all();
 
         if (isset($form['items_image'])) {
-            $fileName =  $request->file('items_image')->getClientOriginalName();
-
-            $generateName = new GenerateImageFileName($fileName);
-            $saveFileName = $generateName->outPutFileName();
-
-            $s3FileName = Storage::disk('s3')->putFile('/',$saveFileName,'public');
-            $imageFileName = Storage::disk('s3')->url($s3FileName);
-            $request->file('items_image')->$imageFileName;
+            $path = Storage::disk('s3')->putFile('/',$form['items_image'],'public');
+            $items->items_image = Storage::disk('s3')->url($path);
 
         } else {
             $items->items_image = null;
@@ -61,7 +55,7 @@ class ItemCreateController extends Controller
 
         $items->fill([
             'items_name' => $form['items_name'],
-            'items_image' => $imageFileName,
+            'items_image' => $form['items_image']->getClientOriginalName(),
             'flowering_time' => $form['flowering_time'],
             'full_length' => $form['full_length'],
             'descriptions' => $form['descriptions'],
